8b82856a8473346f6874f804635be96ae6b6cc84
Nour Hassan <Nour Hassan@DESKTOP-6JIHROM>
2021-02-20 03:26:06 +0200

    bbb

--- CouponSystemProject/src/main/java/app/core/entities/Coupon.java
+++ CouponSystemProject/src/main/java/app/core/entities/Coupon.java
@@ -1,5 +1,6 @@
 package app.core.entities;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -15,26 +16,140 @@ import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
 public class Coupon {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	@ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+	@ManyToOne(cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH })
 	@JoinColumn(name = "company_id")
-    private Company company;
-    @Enumerated(EnumType.STRING)
-    private Category category;
-    private String title;
-    private String description;
-    private Date startDate;
-    private Date endDate;
-    private Integer amount;
-    private Double price;
-    private String image;
-    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
-    @JoinTable(name = "coupon_customer",
-    		joinColumns = @JoinColumn(name = "coupon_id"),
-    		inverseJoinColumns = @JoinColumn(name = "customer_id"))
-    private List<Customer> customers;
+	private Company company;
+	@Enumerated(EnumType.STRING)
+	private Category category;
+	private String title;
+	private String description;
+	private Date startDate;
+	private Date endDate;
+	private Integer amount;
+	private Double price;
+	private String image;
+	@ManyToMany(cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH })
+	@JoinTable(name = "coupon_customer", joinColumns = @JoinColumn(name = "coupon_id"), inverseJoinColumns = @JoinColumn(name = "customer_id"))
+	private List<Customer> customers;
+
+	public Coupon() {
+	}
+
+	public Coupon(Category category, String title, String description, Date startDate, Date endDate, Integer amount,
+			Double price, String image) {
+		this.category = category;
+		this.title = title;
+		this.description = description;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.amount = amount;
+		this.price = price;
+		this.image = image;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Company getCompany() {
+		return company;
+	}
+
+	public void setCompany(Company company) {
+		this.company = company;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public Integer getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Integer amount) {
+		this.amount = amount;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public List<Customer> getCustomers() {
+		return customers;
+	}
+
+	public void setCustomers(List<Customer> customers) {
+		this.customers = customers;
+	}
+
+	public void addCustomer(Customer customer) {
+		if (this.customers == null)
+			this.customers = new ArrayList<Customer>();
+		this.customers.add(customer);
+	}
+
+	@Override
+	public String toString() {
+		return "Coupon [id=" + id + ", companyId=" + company.getId() + ", category=" + category + ", title=" + title
+				+ ", description=" + description + ", startDate=" + startDate + ", endDate=" + endDate + ", amount="
+				+ amount + ", price=" + price + ", image=" + image + "]";
+	}
 
 }
